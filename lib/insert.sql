cdde088aa68c3ab05e445ab96a94243255d332ae
Tam Vu <vutam_a@hotmail.com>
2019-08-31 20:34:54 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -7,6 +7,6 @@ INSERT INTO bears (name) VALUES ("Mr.Chocolate");
 INSERT INTO bears (name) VALUES ("Grinch");
 INSERT INTO bears (name) VALUES ("Wendy");
 INSERT INTO bears (name) VALUES ("Mr.Chocolate");
+binding.pry 
 INSERT INTO bears (name) VALUES ("NULL");
 
-binding.pry 
