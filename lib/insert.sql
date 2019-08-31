30dac1d10d22cf63573177cc878ef25549f494f1
Tam Vu <vutam_a@hotmail.com>
2019-08-31 20:33:02 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,6 @@
 INSERT INTO bear (name) VALUES ("Mr.Chocolate");
+INSERT INTO bear (name) VALUES ("Rowdy");
+INSERT INTO bear (name) VALUES ("Tabitha");
+INSERT INTO bear (name) VALUES ("Sergeant''Brown");
+INSERT INTO bear (name) VALUES ("Melissa");
+INSERT INTO bear (name) VALUES ("Mr.Chocolate");
